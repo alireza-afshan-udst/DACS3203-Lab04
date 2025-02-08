d4157d5d9d405b023a37fca19d240c02a7dc22b1
Alireza Afshan <60304390@udst.edu.qa>
2025-02-08 19:07:56 +0300

    text field and value reflected

--- src/main/java/app/lab4/HomeController.java
+++ src/main/java/app/lab4/HomeController.java
@@ -1,34 +1,48 @@
 package app.lab4;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 
 
 
 public class HomeController {
+
     private NavigationHelper router = new NavigationHelper();
     FXMLLoader loader = new FXMLLoader(
             getClass().getResource("home.fxml")
     );
-
     @FXML
     public Label search;
 
-    @FXML
-    private Label welcomeText;
+    public Label textField;
 
     @FXML
-    protected void onSearchButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Label textLabel;
+
+    @FXML
+    private TextField commandSearchField;
+
+    public void initialize() {
+        commandSearchField.textProperty().addListener((obs, oldText, newText) -> {
+            textLabel.setText(newText);
+        });
     }
 
-    @FXML
+    protected void updateText(ActionEvent event) {
+
+    }
+
     public void onSubmit() {
+
         Stage stage = GitLabApplication.getPrimaryStage();
         router.navigateTo("command.fxml", stage);
     }
